fb573d37a3aeec950b294537e440a118a25b6c23
lleigh <lauracleigh314@gmail.com>
2022-02-12 20:28:37 +0000

    second commit

--- Lodge/views.py
+++ Lodge/views.py
@@ -87,16 +87,3 @@ def room(request):
 
 
 #Authorisation
-
-def see_request(request):
-    text = f"""
-        Some attributes of the HttpRequest object:
-
-        scheme: {request.scheme}
-        path:   {request.path}
-        method: {request.method}
-        GET:    {request.GET}
-        user:   {request.user}
-    """
-
-    return HttpResponse(text, content_type="text/plain")
--- web_project/urls.py
+++ web_project/urls.py
@@ -19,14 +19,14 @@ from django.views.generic.base import TemplateView # new
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf import settings
 from django.conf.urls.static import static
-from core import views as core_views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('lodge/', include("Lodge.urls")),
     path('accounts/', include('django.contrib.auth.urls')),
     path('', TemplateView.as_view(template_name='home.html'), name='home'),
-    path("see_request/", core_views.see_request), # new
+    
 ]
 
 urlpatterns += staticfiles_urlpatterns()
